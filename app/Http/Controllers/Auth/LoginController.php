53c54222eca8bc2233131aee669c359c3a236c0b
RuinsGit <museyibli.ruhin@gmail.com>
2025-04-26 02:05:43 +0400

    ruins

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -15,7 +15,7 @@ class LoginController extends Controller
      */
     public function showLoginForm()
     {
-        return view('auth.login');
+        return view('ai.welcome');
     }
     
     /**
